eaa81cceb64362cb78c4decc82e0d47dcce6d95f
dasha shevchenko <dasha shevchenko>
2017-07-25 19:28:03 -0700

    more fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,8 +118,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
